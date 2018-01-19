945ac1412842ba624957561af4da0462f4868b2a
fancys <fancy553973204@gmail.com>
2018-01-19 11:22:04 +0800

    add messagehandler、iocconfig、utest for api

---
+++ src/BankDirectConnection.Domain/Common/BankServiceConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankDirectConnection.Domain.Common
+{
+    /*===============================================================================================================================
+		Create by Fancy in 2018/1/18 14:20:46
+	===============================================================================================================================*/
+    public class BankServiceConfig
+    {
+        /// <summary>
+        /// 企业前置机
+        /// </summary>
+        public string Termid { get; set; }
+
+        /// <summary>
+        /// 客户端产生的报文编号
+        /// </summary>
+        public string Trnid { get; set; }
+    }
+}
